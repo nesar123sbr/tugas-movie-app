c5526818ca7bc5eb46fbbfed53ad54ae1fe89619
Gada Prima <nesar123siburian@gmail.com>
2021-01-23 21:03:05 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import axios from 'axios';
+import { StyleSheet, Text, View, TextInput, ScrollView, Image, TouchableHighlight, Modal } from 'react-native';
 
 export default function App() {
+  const apiurl ="http://www.omdbapi.com/?i=tt3896198&apikey=d1051794"
+  const [state, setState] = useState({
+    s: "Enter a movie...",
+    results: [],
+    selected: {}
+  });
+
+  const search = () => {
+    axios(apiurl + "&s=" + state.s).then(({ data }) => {
+      let results =data.Search;
+      
+      setState(prevState =>{
+        return { ...prevState, results: results }
+      })
+    })
+  }
+
+const openPopup = IDBCursor => {
+  axios(apiurl + "&i" + id).then(({data}) => {
+    let result = data;
+    
+    console.log(result);
+
+    setState(prevState => {
+      return { ...prevState, selected: result }
+    });
+
+  });
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Movie DB</Text>
+      
+      <TextInput 
+        style={styles.searchbox}
+        onChangeText={text => setState(prevState => {
+          return {...prevState, s: text}
+        })}
+        onSubmitEditing={search}
+        value={state.s}
+      /> 
+
+      <ScrollView style={styles.results}>
+        {state.results.map(result => (
+          <TouchableHighlight key={result.imdbID} onPress={() => openPopup(result.imdbID)} > 
+            <View style={styles.result}>
+              <Image
+                source={{ uri: result.Poster}}
+                style={{
+                  width: '100%',
+                  height: 300,
+                  marginHorizontal: 'auto'
+                }}
+              />
+              <Text style={styles.heading}>{result.Title}</Text>
+            </View>
+          </TouchableHighlight>  
+        ))}
+      </ScrollView>
+
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={(typeof state.selected.Title != "undefined")}
+      >
+        <View style={styles.popup}>
+          <Text style={styles.poptitle}>Hello, World!</Text>
+          <Text style={{marginBottom:20}}>Rating: {state.selected.imdbRating}</Text>
+          <Text>{state.selected.Plot}</Text>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -14,8 +82,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#223343',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 70,
+    paddingHorizontal: 20,
   },
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  searchbox: {
+    fontSize: 20,
+    fontWeight: '300',
+    padding: 20,
+    width: '100%',
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    marginBottom: 40
+  },
+  results: {
+    flex: 1,
+  },
+  result: {
+    flex: 1,
+    width: '100%',
+    marginBottom: 20
+  },
+  heading: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '700',
+    padding: 20,
+    backgroundColor: '#445565'
+  }
+
 });
